9409526252e8f2e6791f64f1931f077cc7cbe920
xebec19 <thakur190420@gmail.com>
2020-12-16 10:29:50 +0530

    set up register end point

--- server.js
+++ server.js
@@ -17,11 +17,11 @@ app.use(express.urlencoded({extended:false}));  //alternative for body-parsar
 app.use(express.json());
 
 
-app.get('/',(req,res) => {
+app.get('/',(req,res) => {  //to check that server is running
 	res.send("Hello World!");
 })
 
-app.get('/users',(req,res) => {
+app.get('/users',(req,res) => {  //to get all users
   
 	db.select('*').from('users')
 	.then(resp => {
@@ -30,5 +30,20 @@ app.get('/users',(req,res) => {
 	.catch(err => res.status(400).json(`An error occured: ${err}`))
 
 })
+
+app.post('/register',(req,res) => {  //to register new users
+
+	const recv = req.body;  //alias for request
+	db  //name of database
+	//following command insert new user into database
+	.insert([{email: recv.email, name: recv.name, password: recv.password }]  
+	, ['email','name'])  //return email and name of new user
+	.into('users')  //name of table
+	.then((resp => {
+		res.status(200).json(resp[0])  //send user as response
+	}))
+	.catch(err => res.status(400).json(`There is an error: ${err}`))
+})
+
 
 app.listen(port ,() => {console.log(`Server is running at ${port}`)})  //to print port number
